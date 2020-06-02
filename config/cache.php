5bd0f39e70f74dc20ce584ce537cdf1ad454d10b
Ramesh Bhatta <ramesh.paig@outlook.com>
2020-06-02 20:29:37 +1000

    Remove unnecessary config

--- config/cache.php
+++ config/cache.php
@@ -73,13 +73,8 @@ return [
         ],
 
         'redis' => [
-          'client' => 'predis',
-          'default' => [
-            'host' => env('REDIS_HOST', 'localhost'),
-            'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', 6379),
-            'database' => 0,
-          ],
+          'client' => 'redis',
+          
 
         ],
 
