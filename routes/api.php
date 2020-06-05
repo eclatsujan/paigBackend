86133e2b12545f1f11690fe26fbe2912f7c76c14
Sujan Prajapati Maharjan <prajapati_sujan1@hotmail.com>
2020-06-05 12:24:26 +1000

    Fix No Email Issue

--- routes/api.php
+++ routes/api.php
@@ -24,23 +24,3 @@ Route::middleware(['auth','cors'])->group(function(){
     Route::get("/list","Front\APIController@getAllListings");
     Route::get("/list/detail/{display_id}","Front\APIController@singleDetail");
 });
-
-
-//Route::get('/reqProperties',function(Request $request){
-//
-////    $request->session()->put('state', $state = Str::random(40));
-//    $client=new \GuzzleHttp\Client([
-//        'verify'=>false
-//    ]);
-//    $response=$client->request("POST","https://paigbackend.test/oauth/token",[
-//        "form_params"=>[
-//            'client_id' => '90af49a5-71ba-479a-a8e2-cc2ad437f9c4',
-//            'client_secret'=>'U91FQj8SWg3kQsHmH0QzYIUsDcICGt9lY1cilDi3',
-//            'redirect_uri' => 'http://paigbuildingservices.com.au/wp-admin',
-//            'grant_type' => 'client_credentials',
-//            'scope' => '*',
-//        ],
-////        'debug'=>true
-//    ]);
-//    var_dump( json_decode((string) $response->getBody(), true));
-//});
