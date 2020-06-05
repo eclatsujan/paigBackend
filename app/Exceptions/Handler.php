3462485b7b0e12ea477b4dba366356354b8f7b4b
Sujan Prajapati Maharjan <prajapati_sujan1@hotmail.com>
2020-06-05 14:08:12 +1000

    Add Error

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -56,9 +56,9 @@ class Handler extends ExceptionHandler
 
     protected function unauthenticated($request, AuthenticationException $exception)
     {
-        var_dump($exception->getMessage());
+//        var_dump();
         if ($request->expectsJson()) {
-            return response()->json(['error' => 'Unauthenticated.'], 401);
+            return response()->json(['error' => $exception->getMessage()], 401);
         }
 
         return redirect()->guest(route('login'));
