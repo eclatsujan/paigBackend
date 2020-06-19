ea787574d8dd4b7e990b3b8e1c4d7354648b7e03
Ramesh Bhatta <ramesh.paig@outlook.com>
2020-06-19 14:03:55 +1000

    Quee moved to web routes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,15 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
+
+use App\Mail\JobStart;
+use Illuminate\Queue\Events\JobFailed;
+use Illuminate\Queue\Events\JobProcessed;
+use Illuminate\Queue\Events\JobProcessing;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Queue;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -31,3 +40,31 @@ Route::middleware("auth")->prefix("admin")->group(function(){
     Route::get("job/run",'Admin\JobController@runLocationListingJobs');
 });
 
+Queue::before(function (JobProcessing $event ) {
+    if($event->job->resolveName()==="API\Jobs\PaigQueue"){
+        Mail::to("sujan.paig@outlook.com,ramesh.paig@outlook.com")
+            ->send(new JobStart());
+    }
+    if($event->job->resolveName()==="App\Jobs\PaigAPIJob"){
+        $msg="The job number ".$event->job->getJobId()." has been started.";
+        Log::channel("paigapi")->info($msg);
+    }
+});
+
+Queue::after(function ( JobProcessed $event ) {
+    if($event->job->resolveName()==="App\Jobs\PaigAPIJob"){
+        $msg="The job number ".$event->job->getJobId()." has been finished.";
+        Log::channel("paigapi")->info($msg);
+    }
+});
+
+Queue::failing(function ( JobFailed $event ) {
+    if($event->job->resolveName()==="API\Jobs\PaigQueue"){
+        Mail::to("sujan.paig@outlook.com,ramesh.paig@outlook.com")
+            ->send(new \App\Mail\JobFailed());
+    }
+    if($event->job->resolveName()==="App\Jobs\PaigAPIJob"){
+        $msg="The job number ".$event->job->getJobId()." has been failed.";
+        Log::channel('paigapi')->error($msg);
+    }
+});
