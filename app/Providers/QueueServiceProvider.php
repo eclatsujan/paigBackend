83e573d3d55bbf240a6e93efa0e96f0a6469dcfe
Ramesh Bhatta <ramesh.paig@outlook.com>
2020-06-20 11:27:36 +1000

    quee moved from web route to quee service provider

--- app/Providers/QueueServiceProvider.php
+++ app/Providers/QueueServiceProvider.php
@@ -34,7 +34,39 @@ class QueueServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+
+
+        Queue::before(function (JobProcessing $event ) {
+            if($event->job->resolveName()==="API\Jobs\PaigQueue"){
+                Mail::to("sujan.paig@outlook.com,ramesh.paig@outlook.com")
+                    ->send(new JobStart());
+            }
+            if($event->job->resolveName()==="App\Jobs\PaigAPIJob"){
+                $msg="The job number ".$event->job->getJobId()." has been started.";
+                Log::channel("paigapi")->info($msg);
+            }
+        });
+        
+        Queue::after(function ( JobProcessed $event ) {
+            if($event->job->resolveName()==="App\Jobs\PaigAPIJob"){
+                $msg="The job number ".$event->job->getJobId()." has been finished.";
+                Log::channel("paigapi")->info($msg);
+            }
+        });
+        
+        Queue::failing(function ( JobFailed $event ) {
+            if($event->job->resolveName()==="API\Jobs\PaigQueue"){
+                Mail::to("sujan.paig@outlook.com,ramesh.paig@outlook.com")
+                    ->send(new \App\Mail\JobFailed());
+            }
+            if($event->job->resolveName()==="App\Jobs\PaigAPIJob"){
+                $msg="The job number ".$event->job->getJobId()." has been failed.";
+                Log::channel('paigapi')->error($msg);
+            }
+        });
         
     }
+
+
 
 }
