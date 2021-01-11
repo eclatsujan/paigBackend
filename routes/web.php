639ace78873460567cfa9254b5f5e1eda0374d5b
Sujan Prajapati Maharjan <prajapati_sujan1@hotmail.com>
2021-01-12 09:34:23 +1100

    Add Test

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ use Illuminate\Support\Facades\Queue;
 |
 */
 
-Auth::routes(['register'=>false]);
+Auth::routes(['register' => false]);
 
 Route::get('/', function () {
     return view('welcome');
@@ -32,12 +32,68 @@ Route::get('/', function () {
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::middleware("auth")->prefix("admin")->group(function(){
-    Route::get("settings","Admin\SettingController@view");
-    Route::post("settings","Admin\SettingController@save");
-    Route::get("clients","Admin\ClientController@viewClients");
-    Route::get("jobs/view",'Admin\JobController@viewJobs');
-    Route::get("job/run",'Admin\JobController@runLocationListingJobs');
-    Route::get("job/test",'Admin\JobController@testSingleJob');
+Route::middleware("auth")->prefix("admin")->group(function () {
+    Route::get("settings", "Admin\SettingController@view");
+    Route::post("settings", "Admin\SettingController@save");
+    Route::get("clients", "Admin\ClientController@viewClients");
+    Route::get("jobs/view", 'Admin\JobController@viewJobs');
+    Route::get("job/run", 'Admin\JobController@runLocationListingJobs');
+    Route::get("job/test", 'Admin\JobController@testSingleJob');
 });
 
+Route::get("test", function () {
+    $domains = [
+        "projects.paigtechnologies.com.au",
+        "paigbuildingservices.com.au",
+        "nationaldisabilityhouses.com.au",
+        "projects.reaprojects.com.au",
+        "buyrenovate.com.au",
+        "townies.com.au",
+        "letsbuyaunit.com.au",
+        "villasinaustralia.com.au",
+        "buyawarehouse.com.au",
+        "businessbroking.com.au",
+        "buyterraces.com.au",
+        "projects.professionalsblacktown.com.au",
+        "projects.goldennest.com.au",
+        "uwp.hashtagportal.com.au",
+        "divinehomes.hashtagportal.com.au",
+        "inspiredpropertyinvesting.hashtagportal.com.au",
+        "squareyards.hashtagportal.com.au",
+        "eikogroup.hashtagportal.com.au",
+        "meridianaustralia.hashtagportal.com.au",
+        "hashtagcustomhomes.com.au",
+        "projects.meridianaustralia.com.au",
+        "hashtagcustombuilds.com.au",
+        "buycommercialrealestate.com.au",
+        "developmentsinrealestate.com.au",
+        "existingrealestate.com.au",
+        "grannyflatrealestate.com.au",
+        "houseandlandproperties.com.au",
+        "newlandestate.com.au",
+        "offtheplanpurchase.com.au",
+        "smsfrealestate.com.au",
+        "builddesigns.com.au",
+        "dualdwellings.com.au",
+        "ddpproperty.hashtagportal.com.au",
+        "projects.simonehomes.com.au",
+        "highincomeproperties.com.au",
+        "ljhookerminto.hashtagportal.com.au",
+        "landandlease.hashtagportal.com.au",
+        "sapphireestateagents.hashtagportal.com.au",
+        "ddpprojects.com.au",
+        "strategicrealty.hashtagportal.com.au",
+        "dualentryproperties.com.au",
+        "www.hashtagregionalhomes.com.au",
+        "landhar.hashtagportal.com.au",
+        "ssrgproperties.hashtagportal.com.au",
+        "novolink.hashtagportal.com.au",
+    ];
+    foreach($domains as $domain){
+        $result=dns_get_record($domain);
+        echo "<pre>";
+        var_dump(checkdnsrr($domain,'A'));
+        print_r($result);
+        echo "</pre>";
+    }
+});
